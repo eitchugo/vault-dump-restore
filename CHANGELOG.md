37a46393c57f99868fb9ffcc484a47fc91f04873
Hugo Cisneiros (Eitch) <hugo.cisneiros@gmail.com>
2022-01-17 21:17:19 -0300

    python 3.6 requirement and bump version to 0.4.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,13 @@
 
 Here you can see a full list of changes between each release.
 
+## 0.4.1
+
+Released on Jan 17th, 2022
+
+* Fixed single quote shell escapes on vault command output
+* Minimum python version is 3.6 instead of 3.8
+
 ## 0.4.0
 
 Released on Oct 28th, 2021
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = (this_directory / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name="vault-dump-restore",
-    version='0.4.0',
+    version='0.4.1',
     description='Dumps keys from a Hashicorp Vault instance to be able to restore it on another instance',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -21,13 +21,14 @@ setup(
     author_email='hugo.cisneiros@gmail.com',
     classifiers=[
         'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3 :: Only',
     ],
     keywords='hashicorp, vault, keys, secrets, backup, restore',
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    python_requires=">=3.8, <4",
+    python_requires=">=3.6, <4",
     install_requires=[
         'hvac==0.11.2'
     ],
